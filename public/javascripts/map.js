ec4e4015c2566e325f76b55b0c65b47f98f49a27
Rickard Laurin <rickard.laurin@gmail.com>
2013-03-16 18:29:39 +0100

    design changes

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -19,12 +19,16 @@ function MapCtrl($scope, $http){
   delete $http.defaults.headers.common['X-Requested-With'];
 
   var poiUrl = 'api/poi?q={query}&lng={lng}&lat={lat}';
-  var mapUrl = "https://maps.googleapis.com/maps/api/staticmap?center={lat},{lng}&zoom={zoom}&scale=2&size=640x640&maptype=terrain&sensor=true";
-      
+  var mapUrl = "https://maps.googleapis.com/maps/api/staticmap?center={lat},{lng}&zoom={zoom}&scale=2&size=640x640&maptype=terrain&sensor=true&style=feature:all%7Csaturation:-100%7Cweight:0.8&style=feature:water%7Clightness:90";
   var compass = new Compass();
 
   var scale = 20000;
   $scope.zoom = 15;
+
+  $scope.updateQuery = function(type) {
+    $scope.query = type;
+    $scope.menuOpen = false;
+  }
 
   $scope.$watch('query', function(){
     console.log('queryChange');
