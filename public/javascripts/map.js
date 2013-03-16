e7cc425115082a58eeae6b592c98c8e5bc26332f
Rickard Laurin <rickard.laurin@gmail.com>
2013-03-16 16:26:59 +0100

    Stuffz

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,20 +1,24 @@
 
-
-
 // http://narmaste.se/Map/JsonQuery?q=brevlada&lng=17.271347045898455&lat=59.23474362209861
 
 
 function MapCtrl($scope, $http){
   $scope.pois = ['poi1', 'poi2'];
-  $scope.query = 'brevlada';
+  $scope.query = undefined;
   $scope.position = undefined;
+  $scope.heading = undefined;
 
   $scope.flatStyle = undefined;
   $scope.mapUrl = undefined;
 
+  $scope.menuOpen = false;
+  $scope.popupOpen = false;
+
+  console.log($scope);
+
   delete $http.defaults.headers.common['X-Requested-With'];
 
-  var poiUrl = 'http://narmaste.se/Map/JsonQuery?q={query}&lng={lng}&lat={lat}';
+  var poiUrl = 'api/poi?q={query}&lng={lng}&lat={lat}';
   var mapUrl = "https://maps.googleapis.com/maps/api/staticmap?center={lat},{lng}&zoom={zoom}&scale=2&size=640x640&maptype=terrain&sensor=true";
       
   var compass = new Compass();
@@ -34,17 +38,19 @@ function MapCtrl($scope, $http){
     }
     bind();
   });
-
+  
+  $scope.$watch('heading', function() {
+    console.log('headingChange');
+  });
 
   compass.onHeadingChange = function(heading){
-    console.log(heading);
-    // bind();
+    $scope.heading = Math.round(heading);
+    console.log('scopeHeading', $scope.heading);
+    document.getElementById('flat').style.webkitTransform = 'rotateZ(' + -heading + 'deg) rotateX(60deg) translate3d(0,0,1px)';
   };
   compass.onPositionChange = function(position){
-    console.log(position);
     $scope.position = position;
   };
-
 
   function bind(){
     if (!$scope.position)
@@ -52,7 +58,6 @@ function MapCtrl($scope, $http){
 
     $http.get(poiUrl.replace('{query}', $scope.query).replace('{lng}', $scope.position.lng).replace('{lat}', $scope.position.lat))
     .success(function(data){
-
       data = data.map(function(poi){
         poi.x = 640 + ( poi.Position.Longitude - $scope.position.lng ) * (zoomLevelScales[$scope.zoom] / 10);
         poi.y = 640 - ( poi.Position.Latitude - $scope.position.lat ) * (zoomLevelScales[$scope.zoom] / 10);
@@ -62,8 +67,8 @@ function MapCtrl($scope, $http){
         return poi;
       });
 
-      console.log('data', data);
       $scope.pois = data;
+      console.log($scope.pois);
     });
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -5,3 +5,18 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Närmaste.se' })
 };
+
+exports.poi = function(req, res) {
+	var request = require('request')
+		, url = require('url')
+		, url_parts = url.parse(req.url, true)
+		, query = url_parts.query;
+
+	request('http://narmaste.se/Map/JsonQuery?q={query}&lng={lng}&lat={lat}'.replace('{query}', query.q).replace('{lng}', query.lng).replace('{lat}', query.lat), function(err, response, body) {
+		if (response.statusCode == 200 && !err) {
+			res.end(body);
+		}
+	})
+
+
+};
